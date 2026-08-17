6200a4f6426ad3958649bb483c9c84be098570e5
Sameeksha <yadavsamiksha729@gmail.com>
2026-08-17 13:57:29 +0530

    Exception-Handle

--- src/main/java/com/spti/controller/PatientController.java
+++ src/main/java/com/spti/controller/PatientController.java
@@ -89,16 +89,24 @@ public class PatientController {
 		   }
 		   
 
-     //frontEnd validation for email exists
     @GetMapping("/check-email")
     public ResponseEntity<Boolean> isEmailExists(@RequestParam String email) {
-       boolean exists = patientService.isEmailExists(email);
-       return ResponseEntity.ok(exists);
+    try {
+        boolean exists = patientService.isEmailExists(email);
+        return ResponseEntity.ok(exists);
+    } catch (IllegalArgumentException e) {
+        return ResponseEntity.badRequest().build();
     }
+}
 
 	@GetMapping("/check-phone")
     public ResponseEntity<Boolean> isPhoneExists(@RequestParam String phoneNumber) {
+		try{
         boolean exists = patientService.isPhoneNumberExists(phoneNumber);
         return ResponseEntity.ok(exists);
+		}
+		catch(IllegalArgumentException e){
+			return ResponseEntity.badRequest().build();
+		}
 }
 }
